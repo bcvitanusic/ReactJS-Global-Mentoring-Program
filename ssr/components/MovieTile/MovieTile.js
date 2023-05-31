0f127e2f4fe850797c5a0e23f971ed3d4ba7fec0
bcvitanusic <branimir.cvitanusic@hotmail.com>
2023-05-31 13:55:12 +0200

    removed unused useefect

--- ssr/components/MovieTile/MovieTile.js
+++ ssr/components/MovieTile/MovieTile.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import './MovieTile.css';
 import MovieCard from './MovieCard';
 import { useMoviesContext } from '../../app/context/movieList';
